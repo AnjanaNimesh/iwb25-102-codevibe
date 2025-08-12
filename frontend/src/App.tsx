5dce3368fc4e175d3a3a363e1da23e7b81e2403a
JanithAberathna <dilshanjanith68@gmail.com>
2025-08-12 15:51:15 +0530

    updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Admin from './layout/Admin';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import Hospital from './layout/Hospital';
+import HospitalDashboard from './pages/hospital/HospitalDashboard';
+import Home from './pages/shared/Home';
+import Donor from './layout/Donor';
+import DonorDashboard from './pages/donor/DonorDashboard';
+import AddHospitals from "./pages/admin/AddHospitals";
+import AddHospitalUsers from "./pages/admin/AddHospitalUsers";
+import ManageDonors from "./pages/admin/ManageDonors";
+import ManageHospitals from "./pages/admin/ManageHospitals";
+import ManageHospitalUsers from "./pages/admin/ManageHospitalUsers";
+import SystemNotifications from "./pages/admin/SystemNotifications";
+import ViewBloodStock from "./pages/admin/ViewBloodStock";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Shared pages */}
+        <Route path="/" element={<Home />} />
+
+        {/* Admin layout & pages */}
+        <Route path="/admin" element={<Admin />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="addHospital" element={<AddHospitals/>} />
+          <Route path="addHospitalUsers" element={<AddHospitalUsers/>} />
+          <Route path="manageHospitals" element={<ManageHospitals/>} />
+          <Route path="manageHosptitalUsers" element ={<ManageHospitalUsers/>}/>
+          <Route path="manageDonors" element={<ManageDonors />} />
+          <Route path="systemNotifications" element={<SystemNotifications/>}/> 
+          <Route path="viewBloodStock" element ={<ViewBloodStock/>} />     
+        </Route>
+
+        {/* Hospital layout & pages */}
+        <Route path="/hospital" element={<Hospital />}>
+          <Route path="dashboard" element={<HospitalDashboard />} />
+        </Route>
+
+        {/* Donor layout & pages */}
+        <Route path="/donor" element={<Donor />}>
+          <Route path="dashboard" element={<DonorDashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
