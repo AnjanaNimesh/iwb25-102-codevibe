b299cd221e5d955109d0bb8e980a39c9085a1b74
JanithAberathna <dilshanjanith68@gmail.com>
2025-08-29 00:56:56 +0530

    updated homepage

--- frontend/src/pages/shared/Home.tsx
+++ frontend/src/pages/shared/Home.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import UserTypesSection from './components/UserTypes'
 import ServicesSection from './components/Services'
 import WhyChooseSection from './components/WhyChooseSection'
-import Footer from './Footer'
+import Footer from './components/Footer'
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
+      
       <HeroSection/>
       <UserTypesSection/>
       <ServicesSection/>
       <WhyChooseSection/>
-      <Footer/>
+    
     </div>
   )
 }
