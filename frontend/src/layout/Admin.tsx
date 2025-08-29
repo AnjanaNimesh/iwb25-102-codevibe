556cec00e5cdb28747e1af45e42940b35c473dd3
JanithAberathna <dilshanjanith68@gmail.com>
2025-08-29 18:59:38 +0530

    updated admin layout

--- frontend/src/layout/Admin.tsx
+++ frontend/src/layout/Admin.tsx
@@ -1,19 +1,53 @@
+// import React from "react";
+// import { Outlet } from "react-router-dom";
+// import AdminSidebar from "../components/AdminSidebar";
+// import AdminNavBar from "../pages/admin/components/AdminNavbar";
+
+// const Admin: React.FC = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen bg-white">
+//       <div className="flex flex-1">
+//         {/* Sidebar */}
+//         <AdminSidebar />
+
+//         {/* Main content */}
+//         <div className="flex-1 overflow-auto">
+//           <div className="bg-gray-50">
+//             <Outlet />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Admin;
+
+
+
+
+
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../components/AdminSidebar";
+import AdminNavBar from "../pages/admin/components/AdminNavbar";
 
 const Admin: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <div className="flex flex-1">
-        {/* Sidebar */}
-        <AdminSidebar />
+    <div className="flex min-h-screen bg-white">
+      {/* Sidebar */}
+      <AdminSidebar />
 
-        {/* Main content */}
+      {/* Main content */}
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-red-50 via-white to-pink-50">
+        {/* Navbar (not fixed) */}
+        <div>
+          <AdminNavBar />
+        </div>
+
+        {/* Page Content */}
         <div className="flex-1 overflow-auto">
-          <div className="bg-gray-50">
-            <Outlet />
-          </div>
+          <Outlet />
         </div>
       </div>
     </div>
@@ -21,3 +55,4 @@ const Admin: React.FC = () => {
 };
 
 export default Admin;
+
